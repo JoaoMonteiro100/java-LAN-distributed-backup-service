43c4d478dac52ef091aaa0784cccfcd37fc708b0
ei11069 <mjmt1994@gmail.com>
2015-03-31 00:51:13 +0100

    Função de hahsing sha256 a funcionar top, na classe Utilities

--- src/Backup.java
+++ src/Backup.java
@@ -1,4 +1,11 @@
-//backup a specific file and send chunks of it to other PCs
-public class Backup {
-	private int potato = 0;
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+
+public class Backup extends Thread{
+	
+	public static void main (String args[]) throws NoSuchAlgorithmException, UnsupportedEncodingException
+	{
+		Utilities u = new Utilities();
+		System.out.println(u.hashing("potato"));
+	}
 }
---
+++ src/Utilities.java
@@ -0,0 +1,22 @@
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.xml.bind.DatatypeConverter;
+
+
+public class Utilities {
+	
+	public String hashing (String key) throws NoSuchAlgorithmException, UnsupportedEncodingException
+	{
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+		md.update(key.getBytes("UTF-8"));
+		byte[] digest = md.digest();
+		
+	    String hex = DatatypeConverter.printHexBinary(digest);
+	    
+	    return hex;
+	}
+
+}
