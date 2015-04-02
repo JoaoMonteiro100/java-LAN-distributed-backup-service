bc32b14e8c8aeab0a9bf61abe920cf4153a34c96
João Monteiro <joaomiguelmonteiro@gmail.com>
2015-04-02 05:37:10 +0100

    New Message function + gets & sets
    Function to divide a received undiscernible message in its different
    componets.

--- src/protocols/Message.java
+++ src/protocols/Message.java
@@ -1,9 +1,16 @@
 package protocols;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 
 public class Message {
-	private byte[] header;
-	private byte[] body;
+	private byte[] header = {0};
+	private byte[] body = {0};
+	
+	String messageType = "";
+	double version = 0.0;
+	char[] fileId = {'0'};
+	byte[] chunkNo = {0};
+	int replicationDeg = 0;
 	
 	//message with all elements (ex: PUTCHUNK)
 	public Message(String messageType, double version, char[] fileId, byte[] chunkNo, int replicationDeg, char[] content) throws UnsupportedEncodingException {
@@ -60,5 +67,87 @@ public class Message {
 		
 		header = concatenatedHeader.getBytes("US_ASCII");
 		body = cont.getBytes("US_ASCII");
+	}
+	
+	//message with no discernible elements (ex: received message to be interpreted)
+	public Message(byte[] message) throws UnsupportedEncodingException {
+		String messageStr = new String(message, "UTF-8");
+		String[] splitMessage = messageStr.split("\\s"); //the spaces divide the components of the message
+		
+		//common elements to all messages
+		messageType = splitMessage[0];
+		version = Double.parseDouble(splitMessage[1]);
+		fileId = splitMessage[2].toCharArray();
+		
+		//if message has at least 4 elements
+		if(messageType != "DELETE") {
+			chunkNo = splitMessage[3].getBytes(Charset.forName("UTF-8"));
+			
+			//if message has 5 elements
+			if(messageType == "PUTCHUNK") {
+				replicationDeg = Integer.parseInt(splitMessage[4]);
+			}
+		}
+		
+		//TODO: CONFIRMAR SE FUNCIONA ASSIM!!!
+		String[] splitBody = messageStr.split("\\r\\n\\r\\n"); //CRLF CRLF divides de body from the header
+		header = splitBody[0].getBytes(Charset.forName("UTF-8"));
+		body = splitBody[1].getBytes(Charset.forName("UTF-8"));
+	}
+
+	public byte[] getHeader() {
+		return header;
+	}
+
+	public void setHeader(byte[] header) {
+		this.header = header;
+	}
+
+	public byte[] getBody() {
+		return body;
+	}
+
+	public void setBody(byte[] body) {
+		this.body = body;
+	}
+
+	public String getMessageType() {
+		return messageType;
+	}
+
+	public void setMessageType(String messageType) {
+		this.messageType = messageType;
+	}
+
+	public double getVersion() {
+		return version;
+	}
+
+	public void setVersion(double version) {
+		this.version = version;
+	}
+
+	public char[] getFileId() {
+		return fileId;
+	}
+
+	public void setFileId(char[] fileId) {
+		this.fileId = fileId;
+	}
+
+	public byte[] getChunkNo() {
+		return chunkNo;
+	}
+
+	public void setChunkNo(byte[] chunkNo) {
+		this.chunkNo = chunkNo;
+	}
+
+	public int getReplicationDeg() {
+		return replicationDeg;
+	}
+
+	public void setReplicationDeg(int replicationDeg) {
+		this.replicationDeg = replicationDeg;
 	}
 }
