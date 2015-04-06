567a03bac4c5317262904cac0872e3e96a566022
ei11069 <mjmt1994@gmail.com>
2015-04-06 19:20:42 +0100

    New commit with the begining of store

--- src/protocols/Restore.java
+++ src/protocols/Restore.java
@@ -1,6 +1,17 @@
 package protocols;
+
+import java.net.MulticastSocket;
+
 //pull all chunks from a file from other PCs to rebuild it
 public class Restore implements Runnable {
+	
+	private final String filename;
+	private final MulticastSocket sendingSocket;
+
+	public Restore(String filename, MulticastSocket sendingSocket) {
+		this.filename = filename;
+		this.sendingSocket = sendingSocket;
+	}
 
 	@Override
 	public void run() {
