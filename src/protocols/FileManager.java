f51fbebfdc607e3aaad635bc943ca559df84d795
João Monteiro <joaomiguelmonteiro@gmail.com>
2015-04-15 20:13:34 +0100

    Added functions for FileManager

--- src/protocols/FileManager.java
+++ src/protocols/FileManager.java
@@ -1,8 +1,11 @@
 package protocols;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
@@ -15,6 +18,7 @@ import java.util.Date;
 public class FileManager {
 	private String backupDir = "backup.txt"; //contains info about backed up files
 	private String restoreDir = "restore.txt"; //contains info about chunks saved
+	private String space = " ";
 	
 	public FileManager() throws UnsupportedEncodingException, FileNotFoundException, IOException {
 		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
@@ -29,12 +33,52 @@ public class FileManager {
 		}
 	}
 	
-	public void addBackup(String filename, char[] fileId, int repDegree, int chunkNo) {
-		
+	private boolean doesBackupExist(String filename) throws FileNotFoundException, IOException {
+		File f = new File(backupDir);
+		if(isFileRegistered(filename, f)) {
+			return true;
+		}
+		return false;
 	}
 	
-	public void addRestore(char[] fileId, int actualReplication, int repDegree, int chunkNo) {
-		
+	private boolean doesRestoreExist(char[] fileId) throws FileNotFoundException, IOException {
+		File f = new File(restoreDir);
+		if(isFileRegistered(fileId.toString(), f)) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean isFileRegistered(String file, File f) throws FileNotFoundException, IOException {
+		if(f.exists() && !f.isDirectory()) {
+			try(BufferedReader br = new BufferedReader(new FileReader(f))) {
+			    for(String line; (line = br.readLine()) != null; ) {
+			        String[] components = line.split(space);
+			        if(components[0].equals(file)) {
+			        	return true;
+			        }
+			    }
+			}
+		}
+		return false;
+	}
+
+	public void addBackup(String filename, char[] fileId, int repDegree, int chunkNo) throws UnsupportedEncodingException, FileNotFoundException, IOException {
+		if(!doesBackupExist(filename)) {
+			try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+		              new FileOutputStream(backupDir), "utf-8"))) {
+			   writer.write(filename + space + fileId.toString() + space + Integer.toString(repDegree) + space + Integer.toString(chunkNo));
+			}
+		}
+	}
+	
+	public void addRestore(char[] fileId, int actualReplication, int repDegree, int chunkNo) throws FileNotFoundException, UnsupportedEncodingException, IOException {
+		if(!doesRestoreExist(fileId)) {
+			try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+		              new FileOutputStream(backupDir), "utf-8"))) {
+			   writer.write(fileId.toString() + space + Integer.toString(actualReplication) + space + Integer.toString(repDegree) + space + Integer.toString(chunkNo));
+			}
+		}
 	}
 	
 	public void removeBackup(String filename) {
